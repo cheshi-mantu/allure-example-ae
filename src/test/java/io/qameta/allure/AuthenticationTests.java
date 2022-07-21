5d6e4eeb2d9e9bff68c0b92f26e714038e64e0da
Egor Ivanov <egor.ivanov@pm.me>
2022-07-21 08:51:41 +0300

    added @Test to successfulAuthUserPassword

--- src/test/java/io/qameta/allure/AuthenticationTests.java
+++ src/test/java/io/qameta/allure/AuthenticationTests.java
@@ -2,19 +2,21 @@ package io.qameta.allure;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
 
+@Feature("Authentication")
+@Owner("egorivanov")
 public class AuthenticationTests {
     @Test
-    @AllureId("91363")
+    @AllureId("1740")
     @DisplayName("Successful auth with username and password")
-    @Tag("smoke")
-    @Feature("Authentication")
-    @Story("Authentication via username and password")
+    @Tags({@Tag("web"), @Tag("smoke"), @Tag("critical")})
     @Microservice("Authentication")
-    @Owner("egorivanov")
+    @Story("Authentication via username and password")
+
     public void successfulAuthUserPassword() {
             step("Open log-in page ", ()->{
                 Allure.attachment("Open page:", "https://testing.productname.org");

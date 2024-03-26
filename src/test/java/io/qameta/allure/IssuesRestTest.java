1c2bd017ff83eea6711cd6d3c0b29f83f20bbd28
Egor Ivanov <cheshi.mantu@protonmail.com>
2024-03-26 18:03:53 +0200

    updated meta and descriptions

--- src/test/java/io/qameta/allure/IssuesRestTest.java
+++ src/test/java/io/qameta/allure/IssuesRestTest.java
@@ -1,21 +1,17 @@
 package io.qameta.allure;
 
-import io.qameta.allure.model.Parameter;
-import io.qameta.allure.Param;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Date;
 import java.util.stream.Stream;
 
-import static io.qameta.allure.Allure.parameter;
 
 @Layer("rest")
 @Owner("egoriavanov")
-@Feature("TestsParameters")
+@Feature("Tests Parameters")
 public class IssuesRestTest {
 
     private static final String OWNER = "allure-framework";
@@ -23,12 +19,12 @@ public class IssuesRestTest {
 
     private final RestSteps steps = new RestSteps();
 
-    @Story("Dynamic excluded param")
-    @Microservice("Repository")
-    @Tags({@Tag("parameter"), @Tag("dynamic"), @Tag("showcase")})
-    @ParameterizedTest(name = "should show dynamic parameter but ignore it in history ID calcs")
+    @Story("Dynamic excluded parameter")
+    @Microservice("Report")
+    @Tags({@Tag("parameter"), @Tag("dynamic"), @Tag("excluded")})
+    @ParameterizedTest(name = "should show dynamic parameter but ignore it in history ID calculations")
     @MethodSource("epochTimestamps")
-    public void shouldDeleteUserNote(@Param(value = "Title", excluded = true) long epochTimestamp) {
+    public void shouldIgnoreDynamicParametersInTesgResults(@Param(value = "Title", excluded = true) long epochTimestamp) {
         Date date = new Date(epochTimestamp);
         String note = date.toString();
         steps.createIssueWithTitle(OWNER, REPO, note);

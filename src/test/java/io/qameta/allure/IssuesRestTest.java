77833b77aa4bdc2f6ddc0ae283b643ed9f3b2670
Egor Ivanov <cheshi.mantu@protonmail.com>
2024-03-26 16:42:43 +0200

    parameters now are generated as epoch timestamp

--- src/test/java/io/qameta/allure/IssuesRestTest.java
+++ src/test/java/io/qameta/allure/IssuesRestTest.java
@@ -5,7 +5,11 @@ import io.qameta.allure.Param;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 import static io.qameta.allure.Allure.parameter;
 
@@ -23,12 +27,18 @@ public class IssuesRestTest {
     @Microservice("Repository")
     @Tags({@Tag("web"), @Tag("regress"), @Tag("second-pipe")})
     @ParameterizedTest(name = "Close issue via api")
-    @ValueSource(strings = {"First Note", "Second Note", "Third Note", "Fourth note"})
-    public void shouldDeleteUserNote(@Param(value = "Title", excluded = true) String title) {
+    @MethodSource("epochTimestamps")
+    public void shouldDeleteUserNote(@Param(value = "Title", excluded = true) long epochTimestamp) {
 //    public void shouldDeleteUserNote(@Param(mode = Parameter.Mode.) String title) {
-        steps.createIssueWithTitle(OWNER, REPO, title);
-        steps.closeIssueWithTitle(OWNER, REPO, title);
+        Date date = new Date(epochTimestamp);
+        String note = date.toString();
+        steps.createIssueWithTitle(OWNER, REPO, note);
+        steps.closeIssueWithTitle(OWNER, REPO, note);
     }
-
+    static Stream<Long> epochTimestamps() {
+        long epochOne = System.currentTimeMillis();
+        long epochTwo = System.currentTimeMillis() + 1000000; // Adding 1000 seconds for the second timestamp
+        return Stream.of(epochOne, epochTwo);
+    }
 
 }

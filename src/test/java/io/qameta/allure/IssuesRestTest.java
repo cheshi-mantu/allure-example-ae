a296b9712dda82bc0f27dc0f88713bd36da08edd
Egor I <egor.ivanov@pm.me>
2024-09-11 21:04:10 +0300

    refactoring

--- src/test/java/io/qameta/allure/IssuesRestTest.java
+++ src/test/java/io/qameta/allure/IssuesRestTest.java
@@ -1,6 +1,7 @@
 package io.qameta.allure;
 
 import io.qameta.allure.model.Parameter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,8 +22,9 @@ public class IssuesRestTest {
     private final RestSteps steps = new RestSteps();
 
     @Story("Simple parameterized test")
+    @DisplayName("Simple parameterised test")
     @Tags({@Tag("parameter"), @Tag("simple")})
-    @ParameterizedTest(name = "Simple parameterized test")
+    @ParameterizedTest(name = "({argumentsWithNames})")
     @ValueSource(strings = {"First Note", "Second Note"})
     public void useCaseParameterizedTest(@Param("Note") String note) {
         steps.createIssueWithTitle(OWNER, REPO, note);
@@ -30,9 +32,10 @@ public class IssuesRestTest {
     }
 
 
-    @Story("Parameterized test with dynamic parameter")
+    @Story("Parameterized test with dynamic parameter marked as excluded")
+    @DisplayName("{arameterised test with dynamic parameter")
     @Tags({@Tag("parameter"), @Tag("dynamic"), @Tag("excluded")})
-    @ParameterizedTest(name = "Dynamic Parameter marked as excluded")
+    @ParameterizedTest(name = "({argumentsWithNames})")
     @Description("@Param(value = \"Title\", excluded = true \n allows having a dynamic parameter which is not being used in hashes to get history ID")
     @MethodSource("epochTimestamps")
     public void useCaseDynamicParameterExcluded(@Param(value = "Title", excluded = true) long epochTimestamp) {
@@ -43,10 +46,11 @@ public class IssuesRestTest {
     }
 
     @Story("Parameterized test with dynamic parameter")
+    @DisplayName("Parameterised test with parameter marked as Hidden")
     @Tags({@Tag("parameter"), @Tag("dynamic"), @Tag("hidden")})
     @Description("@Param(value = \"Title\", mode = Parameter.Mode.HIDDEN \n allows having a dymanic parameter in test, that is hidden from UI and \n" +
             "is not used the history ID calculations")
-    @ParameterizedTest(name = "Dynamic parameter marked as hidden")
+    @ParameterizedTest(name = "({argumentsWithNames})")
     @MethodSource("epochTimestamps")
     public void useCaseHiddenParameterTest(@Param(value = "Title", mode = Parameter.Mode.HIDDEN) long epochTimestamp) {
         Date date = new Date(epochTimestamp);

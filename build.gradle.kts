7b642ea48ba3fa9e9d7d3965c655cff6774754e4
Egor I <egor.ivanov@pm.me>
2024-09-11 21:01:37 +0300

    gradle files updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     java
     id("io.qameta.allure") version "2.9.4"
@@ -8,14 +10,14 @@ version = version
 
 allure {
     report {
-        version.set("2.26.0")
+        version.set("2.18.1")
     }
     adapter {
         autoconfigure.set(true)
         aspectjWeaver.set(true)
         frameworks {
             junit5 {
-                adapterVersion.set("2.26.0")
+                adapterVersion.set("2.18.1")
             }
         }
     }
@@ -36,6 +38,9 @@ tasks.withType(Test::class) {
     systemProperty("junit.jupiter.execution.parallel.config.strategy", "dynamic")
 
     systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
+    testLogging {
+        setEvents(listOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED))
+    }
 }
 
 
@@ -46,7 +51,7 @@ repositories {
 
 dependencies {
     implementation("commons-io:commons-io:2.6")
-    implementation("io.qameta.allure:allure-java-commons:2.26.0")
+    implementation("io.qameta.allure:allure-java-commons:2.28.0")
     implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     implementation("org.junit.jupiter:junit-jupiter-params:5.7.2")

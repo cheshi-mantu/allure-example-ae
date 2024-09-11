6f914e5b657a8c944515b61264eea6c6649a3bbe
Egor I <egor.ivanov@pm.me>
2024-09-11 21:00:15 +0300

    gradle files updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,26 @@
-import io.qameta.allure.gradle.AllureExtension
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-
-    dependencies {
-        classpath("io.qameta.allure:allure-gradle:2.8.1")
-    }
-}
-
-tasks.existing(Wrapper::class) {
-    gradleVersion = "6.8.3"
-    distributionType = Wrapper.DistributionType.ALL
+plugins {
+    java
+    id("io.qameta.allure") version "2.9.4"
 }
 
 group = "io.eroshenkoam"
 version = version
 
-plugins {
-    java
-    maven
-}
-
-apply(plugin = "io.qameta.allure")
-
-configure<AllureExtension> {
-    autoconfigure = true
-    aspectjweaver = true
-    version = "2.16.0"
-
-    useJUnit5 {
-        version = "2.16.0"
+allure {
+    report {
+        version.set("2.18.1")
     }
-
+    adapter {
+        autoconfigure.set(true)
+        aspectjWeaver.set(true)
+        frameworks {
+            junit5 {
+                adapterVersion.set("2.18.1")
+            }
+        }
+    }
 }
 
 tasks.withType(JavaCompile::class) {
@@ -51,7 +38,11 @@ tasks.withType(Test::class) {
     systemProperty("junit.jupiter.execution.parallel.config.strategy", "dynamic")
 
     systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
+    testLogging {
+        setEvents(listOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED, TestLogEvent.PASSED))
+    }
 }
+
 
 repositories {
     mavenCentral()
@@ -59,12 +50,9 @@ repositories {
 }
 
 dependencies {
-    compile("commons-io:commons-io:2.6")
-    compile("io.qameta.allure:allure-java-commons:2.16.0")
-
-    compile("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    compile("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    compile("org.junit.jupiter:junit-jupiter-params:5.7.0")
-
-    testCompile("io.qameta.allure:allure-junit-platform:2.16.0")
+    implementation("commons-io:commons-io:2.6")
+    implementation("io.qameta.allure:allure-java-commons:2.28.0")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    implementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
 }

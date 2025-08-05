56316a50a2361245a7540f9cba28f131e468ae64
Egor I <egor.ivanov@pm.me>
2025-08-05 16:28:12 +0200

    adds comment on the included commands
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- history.sh
+++ history.sh
@@ -1,4 +1,11 @@
+# I need this for I have defined a custom command to execute allure2 and allure3 in my .zshrc file.
+
+#alias allure2='/opt/homebrew/Cellar/allure/2.34.1/bin/allure'
+#alias allure3='/Users/something/.nvm/versions/node/v21.7.3/bin/allure'
+
 source ~/.zshrc
+
+
 rm -rf build/
 rm -rf allure-report/
 

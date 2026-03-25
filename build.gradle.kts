4df2f2f5798ee83e4c3f47e8815cf75152649103
Egor I <egor.ivanov@pm.me>
2026-03-25 16:45:10 +0100

    upd
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ allure {
 }
 
 tasks.withType(JavaCompile::class) {
-    sourceCompatibility = "${JavaVersion.VERSION_21}"
-    targetCompatibility = "${JavaVersion.VERSION_21}"
+    sourceCompatibility = "${JavaVersion.VERSION_17}"
+    targetCompatibility = "${JavaVersion.VERSION_17}"
     options.encoding = "UTF-8"
 }
 
